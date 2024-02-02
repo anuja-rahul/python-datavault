ddd16f5a1071177e7991eeedbc538457a6284c44
popsibro07 <a.r.gunasinghe@gmail.com>
2024-02-02 16:58:46 +0530

    changes to metadata handler.

--- metadata_handler.py
+++ metadata_handler.py
@@ -7,18 +7,17 @@ class MetadataHandler:
 
     __metadata_file = "FileData.json"
     __template_file = {
-        "filenames": [],
-        "passwords": []
+        "FileData": []
     }
     __temp_files = os.listdir()
 
-    def __init__(self, filename: list[str], password: list[bytes]):
+    def __init__(self, filename: str, password: str):
         self.init_env()
 
         self.__temp_filename = filename
-        self.__filenames = []
         self.__temp_password = password
-        self.__passwords = []
+        self.__temp_array = {"filename": self.__temp_filename, "password": self.__temp_password}
+        self.__current_array = []
         self.__json_data = {}
 
         self.save_metadata()
@@ -29,30 +28,25 @@ class MetadataHandler:
             with open(MetadataHandler.__metadata_file, "w") as file:
                 file.write(json.dumps(MetadataHandler.__template_file, indent=2))
 
-    def __set_template(self):
-        self.__json_data["filenames"] = self.__filenames
-        self.__json_data["password"] = self.__password
-
-    @DataLogger.logger
-    def __dump_to_json(self):
-        with open(MetadataHandler.__metadata_file, 'w') as file:
-            file.write(json.dumps(self.__json_data, indent=2))
-
     @DataLogger.logger
     def __load_from_json(self):
         with open(MetadataHandler.__metadata_file, 'r') as file:
             self.__json_data = json.loads(file.read())
 
-        if self.__json_data is not None:
-            self.__filenames = self.__json_data["filenames"]
-            self.__password = self.__json_data["passwords"]
+        if self.__json_data["FileData"] is not None:
+            self.__current_array = self.__json_data["FileData"]
+
+    @DataLogger.logger
+    def __dump_to_json(self):
+        with open(MetadataHandler.__metadata_file, 'w') as file:
+            file.write(json.dumps(self.__json_data, indent=3))
 
     @DataLogger.logger
     def __update_metadata(self):
-        if self.__temp_filename not in self.__filenames:
-            self.__filenames += self.__temp_filename
-        if self.__temp_password not in self.__passwords:
-            self.__passwords += self.__temp_password
+        if self.__temp_array not in self.__current_array:
+            self.__current_array.append(self.__temp_array)
+
+        self.__json_data["FileData"] = self.__current_array
 
     def save_metadata(self):
         self.__load_from_json()
